2800cd6b4d9a7cd369f4ab765971df59b59c459c
ayush-ayushman <ayushmanofayush@gmail..com>
2024-01-11 00:59:30 +0530

    first

--- HotelService/src/main/java/com/ty/hotel/service/HotelService.java
+++
@@ -1,8 +0,0 @@
-package com.ty.hotel.service;
-import java.util.List;
-import com.ty.hotel.entity.Hotel;
-public interface HotelService {
-    Hotel SaveHotal(Hotel hotel);
-    Hotel get(String id);
-    List<Hotel> getAllHotel();
-}
--- UserService/src/main/java/com/ty/userservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ty.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ty.userservice.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
